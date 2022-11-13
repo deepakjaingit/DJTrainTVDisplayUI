9e5c2aa13bb41411edecefe89bf837c690c99160
Deepak Jain <deepakjain@cboinfotech.com>
2022-11-13 19:12:51 +0530

    cmd

---
+++ nodebuild.bat
@@ -0,0 +1,3 @@
+@REM node --max_old_space_size=9096 node_modules/@angular/cli/bin/ng build --project=Silk --configuration production --build-optimizer --base-href /erp/
+node --max_old_space_size=8048 node_modules/@angular/cli/bin/ng build
+
